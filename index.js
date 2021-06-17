d649977fb24d6d4fd8bd94663e23128e6aaf3674
Youngseok Choe <dev.youngseok@gmail.com>
2021-06-17 19:26:17 +0900

    Parse command and argument

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 console.log(process.env.TOKEN);
-//client.login(process.env.TOKEN);
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Latexxy is ON');
+});
+
+client.on('message', message => {
+    const prefix = '!'
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args =  message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift();
+    if (command == 'tex'){
+        tex_input = args.join(' ');
+        console.log(tex_input);
+    }
+})
+
+client.login(process.env.TOKEN);
