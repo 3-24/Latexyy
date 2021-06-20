81e66bde242dfe25d4ec2dd3a7cb4a8ffa7a0de1
Youngseok Choe <dev.youngseok@gmail.com>
2021-06-20 19:59:04 +0900

    Fix prefix & Create shutdown command

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const dotenv = require('dotenv');
 const latexConvert = require('./latexConvert.js');
-dotenv.config();
+const config = require('./config.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -10,7 +9,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    const prefix = '!'
+    const prefix = 'tex.'
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args =  message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift();
@@ -19,7 +18,14 @@ client.on('message', message => {
         latexConvert(tex_input, 10.0).then(outputFileName => {
             message.channel.send({files: [outputFileName]});
         }).catch(error => message.channel.send(error.toString()));
+    } else if (command == "shutdown"){
+        if (message.author.id == config.owner_id){
+            message.channel.send('Shutting down...').then(m => {
+                client.destroy();
+                // TODO: destory the database client 
+            });
+        }
     }
 })
 
-client.login(process.env.TOKEN);
+client.login(config.bot_token);
